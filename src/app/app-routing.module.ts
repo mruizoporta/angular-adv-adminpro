131f0ea8ab4dc5627cefba7cb902c5400feccdc3
Milagros Ruiz Oporta <msruiz@gcp.net>
2022-09-12 15:03:13 -0600

    Fin seccion 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,22 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesRoutingModule } from './pages/pages.router';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 const route:Routes=[
-  {path:'', component:PagesComponent,
-  children:[
-    {path:'dashboard',component:DashboardComponent},
-    {path:'progress',component:ProgressComponent},
-    {path:'grafica1',component:Grafica1Component},
-    {path:'', redirectTo:'/dashboard', pathMatch:'full'}
-  ]}, 
 
-  {path:'register',component:RegisterComponent},
-  {path:'login',component:LoginComponent},
-
- 
+  //path:'/dashboard', PageRouting
+  //path:'/auth', AuthRouting,
+  //path:'/medicos', MedicosRouting
+  //path:'/compras', ComprasRouting
+  {path:'', redirectTo:'/dashboard', pathMatch:'full'},
   {path:'**', component:NopagefoundComponent}
 ]
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(route)],
+  imports: [RouterModule.forRoot(route), PagesRoutingModule, AuthRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
