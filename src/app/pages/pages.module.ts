9a5251cab9639b096f37d236597deca4c05b5a01
Milagros Ruiz Oporta <msruiz@gcp.net>
2022-09-13 17:36:47 -0600

    Seccion 6 parte 2

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
@@ -23,7 +25,8 @@ import {RouterModule} from '@angular/router';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ]
 })
 export class PagesModule { }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -12,4 +12,19 @@ export class ProgressComponent {
  get getPorcentaje(){
   return `${this.progreso}%`;
  }
+
+ cambiarValor(valor: number){
+
+  if (this.progreso>=100 && valor>=0)
+  {
+    return this.progreso=100;    
+  }
+
+  if (this.progreso<=0 && valor<0)
+  {
+    return this.progreso=0;    
+  }
+
+    return this.progreso= this.progreso + valor;
+ }
 }
